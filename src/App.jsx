5835c197cd39293534251964d1dd91a1cce77970
adrianchv01 <adrianchv04@gmail.com>
2025-02-24 17:39:06 -0500

    controlador de eventos

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+
+const App = () => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+  
+  return(
+    <div>
+      <div>
+        {left}
+      </div>
+      <button onClick={handleLeftClick}>
+        LEFT
+      </button>
+      <button onClick={handleRightClick}>
+        Right
+      </button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
+    </div>
+  )
+}
+export default App;
